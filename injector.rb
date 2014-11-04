7cccd0f1939a1d9880eea6a30a85fc9bce29759b
Lorel <lorel@wellatribe.com>
2014-11-04 17:50:35 +0100

    include code for Gemfile generation

--- injector.rb
+++ injector.rb
@@ -1,38 +1,42 @@
 require "ruby_parser"
 require "ruby2ruby"
+require "pp"
 
 class Injector
-    def intialize(); end
+    def initialize( insert_gem_tracking = false )
+        @parser = RubyParser.new
+        @rubier = Ruby2Ruby.new
+        @insert_gem_tracking = insert_gem_tracking
+    end
 
     def browse(directory)
-        require "pp"
         files = Dir[directory + "/*"]
-        parser = RubyParser.new
-        modules = Array.new
-	rubier = Ruby2Ruby.new
-	out_dir = './spooned'
-	Dir.mkdir(out_dir) rescue nil
-        for file in files
+        modules = []
+    	
+    	out_dir = './spooned'
+    	Dir.mkdir(out_dir) rescue nil
+        files.each do |file|
             if File.directory?(file)
                 modules = modules + browse(file)
             elsif file.end_with? ".rb"
                 content = File.read(file)
-                tree = parser.parse(content)
-                modules = modules + browseTree(tree)
-		newFile = out_dir + "/" + File.basename(file)
-		fout = File.new(newFile, "w")
-		fout.write(rubier.process(tree))
-		fout.close
+                tree = @parser.parse(content)
+                insert_init_gem_tracking tree if @insert_gem_tracking
+                modules = modules | browseTree(tree)
+                newFile = out_dir + "/" + File.basename(file)
+                fout = File.new(newFile, "w")
+                fout.write(@rubier.process(tree))
+                fout.close
             end
         end
-        return modules
+        modules
     end
 
     def browseTree(tree)
-        modules = Array.new
-	i = 0
-	tree.each_index do |index|
-	    node = tree[index + i]
+        modules = []
+        i = 0
+        tree.each_index do |index|
+            node = tree[index + i]
             if node.class != Sexp
                 next
             end
@@ -40,23 +44,30 @@ class Injector
                 #TODO handle local require (eg: "diranme + ../my_script.rb") where module_required is a sexp
                 module_required = node[3][1]
                 modules << module_required
-		i = i + 1
-		tree.insert(index + i, s(:call, nil, :puts, s(:call, s(:const, :Gem), :loaded_specs)))
+
+                if @insert_gem_tracking
+                    i = i + 1
+                    tree.insert(index + i, s(:call, nil, :track_required_gems, node[3].deep_clone))
+                end
             else
                 modules = modules + browseTree(node)
             end
         end
-        return modules
+        modules
+    end
+
+    def insert_init_gem_tracking tree
+        s_to_insert = @parser.parse( File.read( File.dirname(__FILE__) + "/track_required_gems.to_inject" ) )
+        tree.insert(1, s_to_insert)
     end
 end
-
 
 directory = ARGV[0]
 if directory == nil
     exit 0
 end
 
-browser = Injector.new()
+browser = Injector.new( true )
 modules = browser.browse(directory)
  puts("MODULES required : ")   	
 
